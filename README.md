57f6270c96d62477db5eaaaf8c102194d1123478
James deBoer <james@huronbox.com>
2013-09-12 00:58:49 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,12 @@
+dart2jsass
+==========
+
+Dart2JS-as-a-service.
+
+Testing
+=======
+
+We are using jasmine-node.
+
+npm install jasmine-node -g
+jasmine-node spec/
---
+++ spec/fetch_spec.js
@@ -0,0 +1,6 @@
+describe('dart fetcher', function() {
+  it('should fetch a url', function() {
+    fetch('http://localhost:9867/base/main.dart');
+    //expect('/base/main.dart')
+  })
+});
