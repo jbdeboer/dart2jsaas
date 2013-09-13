6d32b04c1236981927b12af1dc771d4be8545806
James deBoer <james@huronbox.com>
2013-09-13 14:09:08 -0700

    cacheing dart2js output

--- spec/dart2js_runner_spec.js
+++ spec/dart2js_runner_spec.js
@@ -1,10 +1,22 @@
 var runner = require('../lib/dart2js_runner.js');
+var q = require('q');
 
 
 describe('dart2js runner', function() {
+  var opts;
+
+  beforeEach(function() {
+    opts = {
+      fileOps: {
+        readFile: function(path) { return q('file contents');},
+        execDart2JS: function(dir, mainDart) { return q('dart2js'); }
+      }
+    }
+  });
+
   it('should run dart2js', function(done) {
-    runner.runner({})(true).then(function(js) {
-      expect(js).toEqual(false);
+    runner.runner(opts)(true).then(function(js) {
+      expect(js).toEqual('file contents');
       done();
     });
   })
