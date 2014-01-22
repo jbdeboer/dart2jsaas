7040802e158fb5fc85af1b0109c480fdad943ed7
James deBoer <james@huronbox.com>
2014-01-22 15:52:57 -0800

    0.0.12: Include the dart version in the SHA

--- spec/dartfs_spec.js
+++ spec/dartfs_spec.js
@@ -1,10 +1,16 @@
 var dartfs = require('../lib/dartfs.js');
-
+var q = require('q');
 
 describe('dart filesystem', function() {
   it('should update the filesystem', function(done) {
-    dartfs.dartFs({})({files: []}).then(function(updated) {
+    dartfs.dartFs({
+      fileOps: {
+        execDartVersion: function() {
+          return q("dev version");
+        }
+      }
+    })({files: []}).then(function(updated) {
       done();
-    });
+    }).done();
   })
 });
