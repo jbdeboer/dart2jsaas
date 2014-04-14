838346b3d9d704b8ebbdfee1fbbc5ef269eab00c
James deBoer <james@huronbox.com>
2014-04-14 11:50:03 -0700

    A test for dart2jsCheckedMode

--- spec/dart2js_runner_spec.js
+++ spec/dart2js_runner_spec.js
@@ -4,15 +4,17 @@ var q = require('q');
 
 describe('dart2js runner', function() {
   var opts;
-  var dart2JsCalls;
+  var dart2JsCalls, checkedModeCalls;
 
   beforeEach(function() {
     dart2JsCalls = [];
+    checkedModeCalls = [];
     opts = {
       fileOps: {
         readFile: function(path) { return q('file contents');},
-        execDart2JS: function(dir, mainDart) {
+        execDart2JS: function(dir, mainDart, checkedMode) {
           dart2JsCalls.push(mainDart);
+          checkedModeCalls.push(checkedMode);
           return q('dart2js');
         }
       }
@@ -27,6 +29,7 @@ describe('dart2js runner', function() {
     }).then(function(js) {
       expect(js).toEqual('file contents');
       expect(dart2JsCalls).toEqual(['main.dart']);
+      expect(checkedModeCalls).toEqual([true]); // checked by default
     }).then(done);
   });
 
@@ -39,6 +42,20 @@ describe('dart2js runner', function() {
     }).then(function(js) {
       expect(js).toEqual('file contents');
       expect(dart2JsCalls).toEqual(['a.dart', 'b.dart']);
+    }).then(done);
+  });
+
+
+  it('should support dart2js options', function(done) {
+    opts.dart2jsCheckedMode = false;
+    runner.runner(opts)({
+      changed: true,
+      dir: 'baseDir/',
+      mainDart: ['main2.dart']
+    }).then(function(js) {
+      expect(js).toEqual('file contents');
+      expect(dart2JsCalls).toEqual(['main2.dart']);
+      expect(checkedModeCalls).toEqual([false]);
     }).then(done);
   });
 });
